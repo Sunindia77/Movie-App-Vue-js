101a8441716fc76e07763fd3d534551291669eea
Suraj Bharat Satav <suraj.satav@thepsi.com>
2021-01-31 01:28:03 +0530

    vue js App

---
+++ src/components/Home.vue
@@ -0,0 +1,77 @@
+<template>
+
+  <div class="container-fluid home">
+    <Header> </Header>
+    <Banner class="banner-comp"></Banner>
+
+
+    <TrendingMovies data-aos="fade-up"   data-aos-duration="1000"
+ dataType="umovies" ></TrendingMovies>
+
+    <TrendingMovies data-aos="fade-up"  data-aos-duration="1000"
+  dataType="nowplaying"  ></TrendingMovies>
+  <!--  <TrendingMovies dataType="tmovies" ></TrendingMovies>-->
+    <TrendingMovies data-aos="fade-up"   data-aos-duration="1000"
+ dataType="toprated"   ></TrendingMovies>
+
+
+  
+
+  </div>
+
+
+
+</template>
+
+<script>
+
+import TrendingMovies from './TrendingMovies'
+import axios from 'axios'
+import Banner from './Banner'
+import Header from './Header'
+import 'aos/dist/aos.css'
+
+
+export default {
+  name: 'Home',
+  components:{
+    TrendingMovies,
+    Banner,
+    Header
+    
+  },
+  data () {
+    return {
+
+    }
+  },
+
+  created(){
+
+      if(!this.$store.state.config) this.$store.dispatch('getConfig')
+
+
+
+  },
+
+  mounted(){
+
+  }
+}
+</script>
+
+<style >
+
+
+.home{
+  padding:0px;
+}
+
+@media only screen and (max-width:768px){
+  .banner-comp{
+    display:none;
+  }
+}
+
+
+</style>
